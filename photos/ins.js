c1a52329929f081458dc3e4836214f8bc3a79921
fanqidong <1425434413@qq.com>
2018-09-05 18:21:38 +0800

    Site updated: 2018-09-05 18:21:37

--- photos/ins.js
+++ photos/ins.js
@@ -120,7 +120,7 @@
                     var src = 'https://github.com/fanqidong/hexo-next/blob/master/photos/' + data.link[i];
                     var type = data.type[i];
                     var target = src + (type === 'video' ? '.mp4' : '.jpg');
-                    src += '.jpg';
+                    // src += '.jpg';
 
                     liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                   <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
