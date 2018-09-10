34b219c180da28ea64e5516db2f9a01440065b11
fanqidong <1425434413@qq.com>
2018-09-10 17:26:02 +0800

    Site updated: 2018-09-10 17:26:02

--- photos/ins.js
+++ photos/ins.js
@@ -122,7 +122,7 @@
                     var target = src + (type === 'video' ? '.mp4' : '.jpg');
                     src += '';
                     liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-                  <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
+                  <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + target + '">\
                     <img class="reward-img" data-type="' + type + '" src="' + minSrc + '" itemprop="thumbnail" onload="lzld(this)">\
                   </a>\
                   <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
