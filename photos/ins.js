6917f6c09458091dd068af7bb6970e6e0dabfb6b
fanqidong <1425434413@qq.com>
2018-09-10 16:07:10 +0800

    Site updated: 2018-09-10 16:07:10

--- photos/ins.js
+++ photos/ins.js
@@ -119,9 +119,8 @@
                     var minSrc = 'https://raw.githubusercontent.com/fanqidong/hexo-next/master/min_photos/' + data.link[i];
                     var src = 'https://raw.githubusercontent.com/fanqidong/hexo-next/master/photos/' + data.link[i];
                     var type = data.type[i];
-                    var target = src + (type === 'video' ? '.mp4' : '.jpg');
+                    var target = src + (type === 'video' ? '.mp4' : '');
                     // src += '.jpg';
-
                     liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                   <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
                     <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="' + minSrc + '" itemprop="thumbnail" onload="lzld(this)">\
