533ee6681569d6b767c6040f60264f01cfc51559
fanqidong <1425434413@qq.com>
2018-09-11 14:21:57 +0800

    Site updated: 2018-09-11 14:21:56

--- photos/ins.js
+++ photos/ins.js
@@ -123,8 +123,8 @@
                     src += '';
 
                     liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-                  <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                    <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/img/empty.jpg" itemprop="thumbnail" onload="lzld(this)" data-lzled="true">\
+                  <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + src + '">\
+                    <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="'+minSrc+'" itemprop="thumbnail" onload="lzld(this)" data-lzled="true">\
                   </a>\
                   <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
               </figure>';
